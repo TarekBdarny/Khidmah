f1ac0ad7dd7c0b72d4d1b0f146ce70e5b2e7ba40
Tarek <tarekdevel41@gmail.com>
2025-10-21 16:03:40 +0300

    adding not verified banner

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-import { registerUserToDB } from "@/actions/user.action";
+import { getAuthUser, registerUserToDB } from "@/actions/user.action";
 import AvailableCategories from "@/components/AvailableCategories";
 import BecomeAWorker from "@/components/BecomeAWorker";
 import { CallToAction } from "@/components/CallToAction";
@@ -9,15 +9,24 @@ import Footer05Page from "@/components/footer-05/footer-05";
 import HeroSection from "@/components/hero-06/hero-06";
 // import HeroSection from "@/components/HeroSection";
 import LocaleSwitcher from "@/components/LocaleSwitcher";
+import { Banner1 } from "@/components/NotVerifiedBanner";
 import AuthButtons from "@/components/reuseable/buttons/AuthButtons";
 import WhyToChooseUs from "@/components/WhyToChooseUs";
 import { prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { getTranslations } from "next-intl/server";
-import { redirect } from "next/navigation";
+import { title } from "process";
 
 export default async function Home() {
-  const currUser = await currentUser();
+  const { userId } = await auth();
+  const loggedUser = await getAuthUser(userId || "");
+  const bannerProps = {
+    title: "You are not verified",
+    description: "Verify your account to use all the app features",
+    linkText: "Verify Now",
+    linkUrl: "/onboarding",
+    defaultVisible: loggedUser?.verified === false,
+  };
   const t = await getTranslations("CTA");
   const items = [
     t("feature1"),
@@ -26,22 +35,12 @@ export default async function Home() {
     t("feature4"),
     t("feature5"),
   ];
-  if (currUser) {
+  if (userId) {
     await registerUserToDB();
   }
-  // const { userId } = await auth();
-
-  // if (userId) {
-  //   const user = await prisma.user.findUnique({
-  //     where: { clerkId: userId },
-  //   });
-  //   if (!user) return;
-  //   if (!user?.verified) {
-  //     redirect("/onboarding");
-  //   }
-  // }
   return (
     <main className="min-h-screen">
+      <Banner1 {...bannerProps} />
       <HeroSection />
       <WhyToChooseUs />
       <FAQ02 />
